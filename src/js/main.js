73c01c8d46eafd70817bb8bd760137baba3a8f79
2y2son4 <adrianadelteso@gmail.com>
2021-03-27 23:54:48 +0100

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,189 @@
+/* eslint-disable no-console */
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+
+  const cardArray = [
+    {
+      name: 'ale-01',
+      img: './assets/images/01.jpg',
+    },
+    {
+      name: 'ale-02',
+      img: './assets/images/02.jpg',
+    },
+    {
+      name: 'ale-03',
+      img: './assets/images/03.jpg',
+    },
+    {
+      name: 'ale-04',
+      img: './assets/images/04.jpg',
+    },
+    {
+      name: 'ale-05',
+      img: './assets/images/05.jpg',
+    },
+    {
+      name: 'ale-06',
+      img: './assets/images/06.jpg',
+    },
+    {
+      name: 'ale-07',
+      img: './assets/images/07.jpg',
+    },
+    {
+      name: 'ale-08',
+      img: './assets/images/08.jpg',
+    },
+    {
+      name: 'ale-09',
+      img: './assets/images/09.jpg',
+    },
+    {
+      name: 'ale-10',
+      img: './assets/images/10.jpg',
+    },
+    {
+      name: 'ale-11',
+      img: './assets/images/11.jpg',
+    },
+    {
+      name: 'ale-12',
+      img: './assets/images/12.jpg',
+    },
+    {
+      name: 'ale-01',
+      img: './assets/images/01.jpg',
+    },
+    {
+      name: 'ale-02',
+      img: './assets/images/02.jpg',
+    },
+    {
+      name: 'ale-03',
+      img: './assets/images/03.jpg',
+    },
+    {
+      name: 'ale-04',
+      img: './assets/images/04.jpg',
+    },
+    {
+      name: 'ale-05',
+      img: './assets/images/05.jpg',
+    },
+    {
+      name: 'ale-06',
+      img: './assets/images/06.jpg',
+    },
+    {
+      name: 'ale-07',
+      img: './assets/images/07.jpg',
+    },
+    {
+      name: 'ale-08',
+      img: './assets/images/08.jpg',
+    },
+    {
+      name: 'ale-09',
+      img: './assets/images/09.jpg',
+    },
+    {
+      name: 'ale-10',
+      img: './assets/images/10.jpg',
+    },
+    {
+      name: 'ale-11',
+      img: './assets/images/11.jpg',
+    },
+    {
+      name: 'ale-12',
+      img: './assets/images/12.jpg',
+    },
+  ];
+
+  cardArray.sort(() => 0.5 - Math.random());
+
+  const grid = document.querySelector('.main__grid');
+  const resultDisplay = document.querySelector('.result');
+  const resetBtn = document.querySelector('.reset');
+
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
+  // create board
+
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement('img');
+      card.setAttribute('src', './assets/images/back.png');
+      card.setAttribute('data-id', i);
+      card.setAttribute('class', 'main__grid-img ales');
+      card.addEventListener('click', flipCard);
+      grid.appendChild(card);
+    }
+  }
+
+  // check for matches
+  function checkForMatch() {
+    let cards = document.querySelectorAll('.ales');
+
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (optionOneId === optionTwoId) {
+      alert('Has seleccionado la misma Ale dos veces, closer 🤦');
+      cards[optionOneId].setAttribute('src', './assets/images/back.png');
+      cards[optionTwoId].setAttribute('src', './assets/images/back.png');
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('Eres listísima 🥳');
+      cards[optionOneId].setAttribute('src', './assets/images/blank.png');
+      cards[optionTwoId].setAttribute('src', './assets/images/blank.png');
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      alert('Prueba otra vez, perdedora 🖕');
+      cards[optionOneId].setAttribute('src', './assets/images/back.png');
+      cards[optionTwoId].setAttribute('src', './assets/images/back.png');
+    }
+
+    function ales() {
+      if (cardsWon.length === 1) {
+        return 'Ale';
+      } else {
+        return 'Ales';
+      }
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = 'Puntuación:' + ' ' + cardsWon.length + ' ' + ales();
+
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = 'Lo sabía, tata. Has ganado.';
+    }
+  }
+
+  // flip your card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
+
+  // reset button
+  function resetGame() {
+    location.reload();
+  }
+  resetBtn.addEventListener('click', resetGame);
+
+  createBoard();
+});
+
+console.log('Feliz punteaños, tata.');
